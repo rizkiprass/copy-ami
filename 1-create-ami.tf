5bb1a2ed5f61f17b98d5ff18e409ea26364afb73
rizkiprass <rizkiprass@gmail.com>
2023-07-08 13:27:34 +0700

    copy

--- 1-create-ami.tf
+++ 1-create-ami.tf
@@ -1,9 +1,35 @@
-resource "aws_ami_from_instance" "example" {
-  name               = "tf-ami"
-  source_instance_id = "i-09406c9b3998a0011"
+resource "aws_ami_from_instance" "server-1" {
+  name                    = "server-1"
+  source_instance_id      = "i-0d7402fb2dc61da16"
   snapshot_without_reboot = "true"
+
+  tags = {
+    Name = "server-1",
+    Terraform = "Yes",
+    Project = "copy-ami"
+  }
 }
 
-#output "ami_id" {
-#  value = aws_ami_from_instance.example.id
-#}
+resource "aws_ami_from_instance" "server-2" {
+  name                    = "server-2"
+  source_instance_id      = "i-06bc39991c41b1576"
+  snapshot_without_reboot = "true"
+
+  tags = {
+    Name = "server-2",
+    Terraform = "Yes",
+    Project = "copy-ami"
+  }
+}
+
+resource "aws_ami_from_instance" "server-3" {
+  name                    = "server32"
+  source_instance_id      = "i-0be13665461f5fea7"
+  snapshot_without_reboot = "true"
+
+  tags = {
+    Name = "server-3",
+    Terraform = "Yes",
+    Project = "copy-ami"
+  }
+}
--- 3-copyto-dest-region.tf
+++ 3-copyto-dest-region.tf
@@ -1,12 +1,44 @@
-resource "aws_ami_copy" "dest" {
-  name              = "terraform-example"
-  description       = "A copy of ami-xxxxxxxx"
-  source_ami_id     = aws_ami_copy.test.id
+resource "aws_ami_copy" "cp-server-1" {
+  name              = "server-1"
+  description       = "A copy of ${aws_ami_from_instance.server-1.id}"
+  source_ami_id     = aws_ami_from_instance.server-1.id
   source_ami_region = "us-west-2"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "server-1",
+    Terraform = "Yes",
+    Project = "copy-ami"
   }
 
-  provider = "dest"
+  provider = aws.dest
+}
+
+resource "aws_ami_copy" "cp-server-2" {
+  name              = "server-2"
+  description       = "A copy of ${aws_ami_from_instance.server-2.id}"
+  source_ami_id     = aws_ami_from_instance.server-3.id
+  source_ami_region = "us-west-2"
+
+  tags = {
+    Name = "server-2",
+    Terraform = "Yes",
+    Project = "copy-ami"
+  }
+
+  provider = aws.dest
+}
+
+resource "aws_ami_copy" "cp-server-3" {
+  name              = "server-3"
+  description       = "A copy of ${aws_ami_from_instance.server-3.id}"
+  source_ami_id     = aws_ami_from_instance.server-3.id
+  source_ami_region = "us-west-2"
+
+  tags = {
+    Name = "server-3",
+    Terraform = "Yes",
+    Project = "copy-ami"
+  }
+
+  provider = aws.dest
 }
