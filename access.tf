2c90054d4f7f91c0ad5cf82f508e5aa150b418b1
rizkiprass <rizkiprass@gmail.com>
2023-07-08 13:39:52 +0700

    fix access

--- access.tf
+++ access.tf
@@ -1,8 +1,8 @@
 //Source Region
 provider "aws" {
   region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 //Destination Region
@@ -10,6 +10,21 @@ provider "aws" {
   alias = "dest"
 
   region     = "us-east-1"
-#  access_key = ""
-#  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
+
+terraform {
+  cloud {
+    hostname = "app.terraform.io" # Optional for TFC
+    organization = "pras"
+
+    workspaces {
+      name = "copy-ami"
+    }
+  }
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
